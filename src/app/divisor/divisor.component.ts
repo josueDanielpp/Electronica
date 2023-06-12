59fbdd2c362771910ab12d4b9f3ee4c8917abd6d
danielpredel <danielpreciadodelgadillo@gmail.com>
2023-06-12 09:31:59 -0600

    Calculos de resistencias a voltajes divisor

--- src/app/divisor/divisor.component.ts
+++ src/app/divisor/divisor.component.ts
@@ -10,16 +10,23 @@ export class DivisorComponent {
   form2Habilitado:boolean = true;
   // Valores 1er formulario:
   vcc: number = 0;
-  rb: number = 0;
+  re: number = 0;
   rc: number = 0;
+  r1: number = 0;
+  r2: number = 0;
   beta: number = 0;
+  rth: number = 0;
+  vth: number = 0;
 
   // Valores 2do formulario:
-  vrb: number = 0;
+  vre: number = 0;
   vrc: number = 0;
   vce: number = 0;
+  vr1: number = 0;
+  vr2: number = 0;
   ib: number = 0;
   ic: number = 0;
+  ie: number = 0;
 
   desactivarFormulario(form:number):void{
     // Si dio clic al formulario 1 se desactiva el 2do formulario
@@ -37,14 +44,18 @@ export class DivisorComponent {
     this.form1Habilitado = true;
     this.form2Habilitado = true;
     this.vcc = 0;
-    this.rb = 0;
+    this.re = 0;
     this.rc = 0;
+    this.r1 = 0;
+    this.r2 = 0;
     this.beta = 0;
-    this.vrb = 0;
+
+    this.vre = 0;
     this.vrc = 0;
     this.vce = 0;
     this.ib = 0;
     this.ic = 0;
+    this.ie = 0;
   }
 
   validaValoresForm1(transistor:string):void{
@@ -54,21 +65,49 @@ export class DivisorComponent {
   }
 
   calcularForm1():void{
-    // Calcular corriente de IB:
-    this.ib = (this.vcc-0.7)/(this.rb*1000);
-    this.ib *=1000000; 
+    // // Calcular corriente de IB:
+    // this.ib = (this.vcc-0.7)/(this.rb*1000);
+    // this.ib *=1000000; 
 
-    // Calcular corriente de IC:
-    this.ic = this.beta*this.ib;
+    // // Calcular corriente de IC:
+    // this.ic = this.beta*this.ib;
+    // this.ic /= 1000;
+    // // Calcular VCE
+    // this.vce = this.vcc - (this.rc*this.ic);
+
+    // // Calcular VRC
+    // this.vrc = this.vcc - this.vce;
+
+    // // Calcular VRB
+    // this.vrb = this.vcc - 0.7;
+
+    // Calcular resitencia de Thevenin
+    this.rth = this.r1 * this.r2 / (Number(this.r1) + Number(this.r2));
+    console.log(this.rth);
+
+    // Calcular Voltaje de Thevenin
+    this.vth = this.vcc * this.r2 / (Number(this.r1) + Number(this.r2));
+    console.log(this.vth);
+
+    // Calcular Corriente IB
+    this.ib = (this.vth - 0.7) / (this.rth + (Number(this.beta) + Number(1)) * this.re);
+    this.ib *= 1000;
+
+    // Calcular Corriente IC
+    this.ic = this.beta * this.ib;
     this.ic /= 1000;
-    // Calcular VCE
-    this.vce = this.vcc - (this.rc*this.ic);
 
-    // Calcular VRC
-    this.vrc = this.vcc - this.vce;
+    // Calcular Corriente IE
+    this.ie = this.ic + this.ib / 1000;
 
-    // Calcular VRB
-    this.vrb = this.vcc - 0.7;
+    // Calcular Voltaje VRC
+    this.vrc = this.ic * this.rc;
+
+    // Calcular Voltaje VRE
+    this.vre = this.ie * this.re;
+
+    // Calcular Voltaje VCE
+    this.vce = this.vcc - this.vrc - this.vre;
   }
 
 
@@ -79,20 +118,20 @@ export class DivisorComponent {
   }
 
   calcularForm2():void{
-    // Calcular RB
-    this.rb = this.vrb/(this.ib/1000000); 
-    this.rb/=1000; // Convertir a Kilo-Ohms
-    console.log(this.rb);
+    // // Calcular RB
+    // this.rb = this.vrb/(this.ib/1000000); 
+    // this.rb/=1000; // Convertir a Kilo-Ohms
+    // console.log(this.rb);
 
-    // Calcular RC
-    this.rc = this.vrc/(this.ic/1000);
-    this.rc /= 1000; // Convertir a Kilo-Ohms
-    console.log(this.rc);
+    // // Calcular RC
+    // this.rc = this.vrc/(this.ic/1000);
+    // this.rc /= 1000; // Convertir a Kilo-Ohms
+    // console.log(this.rc);
 
-    this.vcc = Number(this.vrc) + Number(this.vce);
-    console.log(this.vcc);
+    // this.vcc = Number(this.vrc) + Number(this.vce);
+    // console.log(this.vcc);
 
-    this.beta = (this.ic/1000)/(this.ib/1000000);
-    console.log(this.beta);
+    // this.beta = (this.ic/1000)/(this.ib/1000000);
+    // console.log(this.beta);
   }
 }
